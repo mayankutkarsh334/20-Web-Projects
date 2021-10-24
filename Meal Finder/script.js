99fe923002b102924c919246a24859e8a7d0137f
mayank334 <mayankutkarsh334@gmail.com>
2021-10-24 11:03:07 +0530

    24/10/2021

---
+++ Meal Finder/script.js
@@ -0,0 +1,46 @@
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const singleMealsEl = document.getElementById("single-meal");
+
+//search meal and fetch from api
+function searchMeal(e) {
+  e.preventDefault();
+  //clear single meal
+  singleMealsEl.innerHTML = "";
+  //get search term
+  const term = search.value;
+  //check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+                    <div class="meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
+                `
+            )
+            .join("");
+        }
+      });
+    //clear search text
+    search.value = "";
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+//Event Listener
+submit.addEventListener("submit", searchMeal);
